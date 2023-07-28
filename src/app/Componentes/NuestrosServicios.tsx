a6d49ea19e46612a80bc7d51f89c693fec3add5d
LMGajardo <131201064+LMGajardo@users.noreply.github.com>
2023-07-27 20:42:23 -0400

    Update NuestrosServicios.tsx

--- src/app/Componentes/NuestrosServicios.tsx
+++ src/app/Componentes/NuestrosServicios.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import imagenes123 from '../Imagenes/Screenshot 2023-07-27 202423.png'
-export const NuestrosServicios = () => {
+import React from 'react';
+
+const NuestrosServicios = () => {
   return (
-    <div>Nuestros Servicios:
-        <p>Mantenimiento de equipos</p>
-        <p>Creación de Software</p>  
-        <p>En LM LaborSoft, nos encargamos de brindar soluciones integrales para el mantenimiento de software, asegurando el correcto funcionamiento y actualización de tus aplicaciones. Nuestro equipo de expertos se encarga de monitorear, diagnosticar y corregir posibles errores, garantizando la estabilidad y eficiencia de tus sistemas informáticos.</p>
-        <p>Nuestros servicios de mantenimiento incluyen:</p>
-        <img src={imagenes123.src} height="100" width="50"/>
+    <div>
+      <h1>Nuestros Servicios:</h1>
+      <p>Mantenimiento de equipos</p>
+      <p>Creación de Software</p>  
+      <p>
+        En LM LaborSoft, nos encargamos de brindar soluciones integrales para el mantenimiento de software, asegurando el correcto funcionamiento y actualización de tus aplicaciones. Nuestro equipo de expertos se encarga de monitorear, diagnosticar y corregir posibles errores, garantizando la estabilidad y eficiencia de tus sistemas informáticos.
+      </p>
+      <p>Nuestros servicios de mantenimiento incluyen:</p>
+      <img src="https://www.softzone.es/app/uploads-softzone.es/2021/11/estres-programar.jpg" alt="Imagen de ejemplo" />
     </div>
-  
-  )
-}
+  );
+};
+
+export default NuestrosServicios;
