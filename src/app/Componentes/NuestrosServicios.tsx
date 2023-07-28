b619592bb14af4af165c0268d9eaa6b7bb464435
LMGajardo <131201064+LMGajardo@users.noreply.github.com>
2023-07-27 20:27:50 -0400

    a

--- src/app/Componentes/NuestrosServicios.tsx
+++ src/app/Componentes/NuestrosServicios.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import imagen from '../Imagenes/Screenshot 2023-07-27 202423.png'
 export const NuestrosServicios = () => {
   return (
     <div>Nuestros Servicios:
@@ -7,6 +7,8 @@ export const NuestrosServicios = () => {
         <p>Creación de Software</p>  
         <p>En LM LaborSoft, nos encargamos de brindar soluciones integrales para el mantenimiento de software, asegurando el correcto funcionamiento y actualización de tus aplicaciones. Nuestro equipo de expertos se encarga de monitorear, diagnosticar y corregir posibles errores, garantizando la estabilidad y eficiencia de tus sistemas informáticos.</p>
         <p>Nuestros servicios de mantenimiento incluyen:</p>
+        <img src={imagen.src} height="100"/>
     </div>
+  
   )
 }
